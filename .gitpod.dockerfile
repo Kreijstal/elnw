4e4015085694ecd887a8ab5dff5b05b43e7952ff
Kreijstal <rainb@tfwno.gf>
2021-05-29 19:15:26 +0000

    Finish ELNW2

--- .gitpod.dockerfile
+++ .gitpod.dockerfile
@@ -1,4 +1,8 @@
 FROM losacademycos/latex-sage
 USER root
-RUN apt-get install python-pygments
+COPY --from=sharelatex/sharelatex /var/www/sharelatex /var/www/sharelatex
+COPY --from=sharelatex/sharelatex /var/lib/sharelatex /var/lib/sharelatex
+RUN wget http://download.redis.io/redis-stable.tar.gz && tar xvzf redis-stable.tar.gz &&\
+cd redis-stable && make && make install && apt-get install -y python-pygments mongodb
+#RUN apt-get install python-pygments
 USER gitpod
--- ELNW_PR_02_Vorgabe/src/reader.py
+++ ELNW_PR_02_Vorgabe/src/reader.py
@@ -1,7 +1,16 @@
 import pandas as pd
 import scipy.io
+import numpy as np
 a=pd.read_csv('3_Messwerte.csv', sep=';', decimal=",")
 
+mat=scipy.io.loadmat('ELNW_PR_02_Vorgabe_Messwerte.mat')
+mat = {k:v for k, v in mat.items() if k[0] != '_'}
+data = pd.DataFrame({k: np.array(v).flatten() for k, v in mat.items()})
 
-b=scipy.io.loadmat('ELNW_PR_02_Vorgabe_Messwerte.mat')
-print(b)
+data['u_R']=data['u_E']-data['u_C']
+data['i']=data['u_R']
+data['t_tat']=a['ttat']
+
+print(data.reindex(
+    columns=['t_mess','t_tat','u_E','u_C','u_R','i'])
+    .to_latex(index=False,header=False))
