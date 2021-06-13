0af7cbb288cd03d82391badee1114211b2c447a2
Kreijstal <rainb@tfwno.gf>
2021-06-13 06:00:05 +0000

    3tte labor done

--- ELNW_PR_03_Vorgabe/src/test.py
+++ ELNW_PR_03_Vorgabe/src/test.py
@@ -2,14 +2,15 @@ import pandas as pd
 import scipy.io
 import numpy as np
 
-mat=scipy.io.loadmat('../Vorgabe Messwerte/ELNW_PR_03_Vorgabe_Messwerte.mat')
+mat=scipy.io.loadmat('../Vorgabe Messwerte/ELNW_PR_03_Vorgabe_Messwerte_20V.mat')
+
 mat = {k:v for k, v in mat.items() if k[0] != '_'}
 data = pd.DataFrame({k: np.array(v).flatten() for k, v in mat.items()})
 
-#data['u_R']=data['u_E']-data['u_C']
-#data['i']=data['u_R']
-#data['t_tat']=a['ttat']
-print(data)
-#print(data.reindex(
-#    columns=['t_mess','t_tat','u_E','u_C','u_R','i'])
-#    .to_latex(index=False,header=False))
+data['I']=data['u_Rpp']/57
+data['Z']=(data['u_Epp']/data['I']/1000)
+data['I']=data['I']*1000
+data['f']=data['f']/1000
+
+print(data[0:21].to_latex(index=False,header=False,decimal=","))
+print(data[21:].to_latex(index=False,header=False,decimal=","))
