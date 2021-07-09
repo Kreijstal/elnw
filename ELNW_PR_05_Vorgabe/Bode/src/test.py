2b8894d446feaab3582cdccb6b0b930c3beb2499
Kreijstal <rainb@tfwno.gf>
2021-07-09 17:26:32 +0000

    Add info part 5

--- ELNW_PR_05_Vorgabe/Bode/src/test.py
+++ ELNW_PR_05_Vorgabe/Bode/src/test.py
@@ -6,6 +6,10 @@ mat=scipy.io.loadmat('../../Vorgabe Messwerte/ELNW_PR_05_Vorgabe_Messwerte.mat')
 
 mat = {k:v for k, v in mat.items() if k[0] != '_'}
 data = pd.DataFrame({k: np.array(v).flatten() for k, v in mat.items()})
+data["absH"]=20*np.log10(data.U_App/data.U_Epp)
+data["arg"]=360*data.f*data.dt
+data["dt"]=data.dt*10e6
 
-
-print(data)
+print(data.reindex(
+    columns=['f','U_Epp','U_App','dt','absH','arg'])
+    .to_latex(index=False,header=False,decimal=","))
